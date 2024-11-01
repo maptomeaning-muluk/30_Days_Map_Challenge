3d4b87b79448ce3f9720bf16cc93f8de11ddc409
maptomeaning-muluk <mulukakshay1@gmail.com>
2024-11-01 23:03:32 +0530

    Zoom level

--- 1_Point/GI_tags_MaH.py
+++ 1_Point/GI_tags_MaH.py
@@ -74,7 +74,7 @@ with col1:
 
 with col2:
     # Initialize the map
-    Map = fl.Map(location=[19.8460931639768, 75.9036387165641], tiles='Cartodb Positron', zoom_start= 8)
+    Map = fl.Map(location=[19.8460931639768, 75.9036387165641], tiles='Cartodb Positron', zoom_start= 7)
 
     # Add state boundary layer with styling
     state_style_function = lambda x: {
