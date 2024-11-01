2f2722f0918eb560b9a318b62d7170eb8f1f295d
Muluk Akshay <131377901+maptomeaning-muluk@users.noreply.github.com>
2024-11-01 23:11:00 +0530

    Update GI_tags_MaH.py

--- 1_Point/GI_tags_MaH.py
+++ 1_Point/GI_tags_MaH.py
@@ -74,7 +74,7 @@ with col1:
 
 with col2:
     # Initialize the map
-    Map = fl.Map(location=map_center, tiles='Cartodb Positron', zoom_start= 7)
+    Map = fl.Map(location=map_center, tiles='Cartodb Positron', zoom_start= 6)
 
     # Add state boundary layer with styling
     state_style_function = lambda x: {
